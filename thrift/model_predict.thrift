29761f4d0033f4f0fb54ebc4187f4c5682743cc1
wangcenhan <wangcenhan@xiaoniuhy.com>
2020-11-18 21:30:37 +0800

    update

--- thrift/model_predict.thrift
+++ thrift/model_predict.thrift
@@ -4,7 +4,9 @@ include 'nx_adp.thrift'
 
 
 service ModelPredictService{
+   # 线上接口
    binary predictPb( 1: binary request_msg );
+   # 测试接口
    double predict( 1: map<string,string> row );
    list<double> predictList( 1: list<map<string,string>> rows );
 
